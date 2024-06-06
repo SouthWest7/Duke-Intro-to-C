23a06546cddc850e877b19496c00d0b6162fb8e7
am1ao <1403572259@qq.com>
2024-06-06 18:13:19 +0800

    Did assignment 18

---
+++ assignment/18_reverse_str/reverse.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char * str) {
+    //WRITE ME!
+    if (!str) {
+        return;
+    }
+    size_t length = 0;
+    char* p = str;
+
+    while (*p != '\0') {
+        length++;
+        p++;
+    }
+
+    for (size_t i = 0; i < length / 2; i++) {
+        char temp = str[i];
+        str[i] = str[length - 1 - i];
+        str[length - 1 - i] = temp;
+    }
+}
+
+int main(void) {
+    char str0[] = "";
+    char str1[] = "123";
+    char str2[] = "abcd";
+    char str3[] = "Captain's log, Stardate 42523.7";
+    char str4[] = "Hello, my name is Inigo Montoya.";
+    char str5[] = "You can be my wingman anyday!";
+    char str6[] = "Executor Selendis! Unleash the full power of your forces! There may be no tomorrow!";
+    char * array[] = {str0, str1, str2, str3, str4, str5, str6};
+    for (int i = 0; i < 7; i++) {
+        reverse(array[i]);
+        printf("%s\n", array[i]);
+    }
+    return EXIT_SUCCESS;
+}
